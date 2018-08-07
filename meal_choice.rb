eff8306fcac7d604f26bf5b9207d9cc7ebfba448
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-07 16:20:17 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -3,16 +3,16 @@ def breakfast(food = "frosted_flakes")
   puts "Morning is the best time for #{food}!"
 end
 
-breakfast()
+breakfast(waffles)
   
 def lunch(food_1 = "grilled_cheese")
   puts "Lunch is the best time for #{food_1}!"
 end 
 
-lunch()
+lunch(hot dogs)
 
 def dinner(food_2 = "salmon")
   puts "Dinner is the best time for #{food_2}!"
 end
 
-dinner() 
+dinner(pizza) 
